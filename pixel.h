f4e05280fc96c129fad8422c5220a53ff95e975a
Jaelson <jaelsonjunior99@gmail.com>
2016-08-04 10:38:17 -0300

    Update pixel class.
    Update PutPixel function amd class constructor.

--- pixel.h
+++ pixel.h
@@ -6,16 +6,20 @@
 class Pixel
 {
 public:
+	int column;
+	int row;
 	int offset; //absolute position each pixel
 
-	Pixel();
+	Pixel(int x, int y);
 	~Pixel();
 	int Offset(int column, int row);
 	void PutPixel(int x, int y, int RGBA[4]);	
 };
 
-Pixel::Pixel()
+Pixel::Pixel(int x, int y)
 {
+	column = x;
+	row = y;
 	//std::cout << "Pixel is being created" << std::endl;
 }
 
@@ -26,19 +30,25 @@ Pixel::~Pixel()
 
 int Pixel::Offset(int column, int row)
 {
-	return column * 4 + row * IMAGE_WIDTH * 4;
+	return (column  + row * IMAGE_WIDTH) * 4;
 }
 
 
 void Pixel::PutPixel(int x, int y, int RGBA[4])
 {
-	offset = Offset(x, y);
-
-	for (int i = 0; i < 4; i++)
+	if ((x >= 0 && x < IMAGE_WIDTH) && (y >= 0 && y < IMAGE_HEIGHT))
 	{
-		FBptr[offset + i] = RGBA[i];
-	}
+		offset = Offset(x, y);
 
+		for (int i = 0; i < 4; i++)
+		{
+			FBptr[offset + i] = RGBA[i];
+		}
+	}
+	else
+	{
+		std::clog << "Invalid PutPixel Position : [ x:" << x << " , y:" << y << " ]" << std::endl;
+	}
 }
 
 #endif // _PIXEL_H_
