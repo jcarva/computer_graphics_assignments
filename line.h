35b2175da3a50ceca67977da58d50674abba4775
Jaelson <jaelsonjunior99@gmail.com>
2016-08-05 10:58:20 -0300

    Update DrawLine function.
    Create some variables to support octante transformations and generalize the Bresenham's Algorithm.

--- line.h
+++ line.h
@@ -1,14 +1,17 @@
 #ifndef _LINE_H_
 #define _LINE_H_
 
+#include <iostream>
+#include <string>
 #include "pixel.h"
+#include <vector>
 
 class Line
 {
 public:
 	Line();
 	~Line();
-	void DrawLine(Pixel initial, Pixel final, int color[4]);
+	void DrawLine(std::string line_name, Pixel initial, Pixel final, int color[4]);
 };
 
 Line::Line()
@@ -21,19 +24,61 @@ Line::~Line()
 	//std::cout << "Line is being deleted" << std::endl;
 }
 
-void Line::DrawLine(Pixel initial, Pixel final, int color[4])
+void Line::DrawLine(std::string line_name, Pixel initial, Pixel final, int color[4])
 {	
-	Pixel reference_pixel = Pixel(0,0);
+
+	/*
+		Pixels vector backup to do octante transformations.
+	*/
+	std::vector<Pixel> buffer_line;
+
+
+	/*
+		Reference Pixel to draw the line, it will be
+		to puted a lot of time in different coordenates.
+	*/
+	Pixel line_reference_pixel = Pixel(0,0);
+
+
+	/*
+		Respresentation of the reference and
+		complementary axes. They are necessary to
+		generalize the	Bresenham Algorithm to any
+		octante, so we need	change the 'while' stop
+		condition, and do octante transformations.
+	*/
+	int reference_axis;
+	int complementary_axis;
+
+	/*
+		Firsts Coordinates to PutPixel if we use
+		the Bresenham Algorithm's base case.
+	*/
+	int x = initial.column;
+	int y = initial.row;
+
+
+	/*
+		Beyonds line coordenates to generalize the
+		Bresenham Algorithm to any octante, so we need
+		change the 'while' stop condition.
+	*/
+	int x_limit;
+	int y_limit;
 
 	int deltaX = final.column - initial.column;
 	int deltaY = final.row - initial.row;
 	int d = (2 * deltaY) - deltaX;
 	int increase_e = 2 * deltaY;
 	int increase_ne = 2 * (deltaY - deltaX);
-	int x = initial.column;
-	int y = initial.row;
 
-	reference_pixel.PutPixel(x,y,color);
+	std::clog << "###################" ;
+	std::clog << std::endl << "LINE: "<< line_name << std::endl;
+	std::clog << "dx: " << deltaX << std::endl << "dy: " << deltaY << std::endl;
+	std::clog << "increase_e: " << increase_e << std::endl << "increase_ne: " << increase_ne << std::endl << std::endl;
+
+	std::clog << "d: " << d << ", x: " << x  << ", y: " << y << std::endl;
+	line_reference_pixel.PutPixel(x,y,color);
 
 	while(x < final.column) {
 		if (d <= 0)
@@ -47,7 +92,8 @@ void Line::DrawLine(Pixel initial, Pixel final, int color[4])
 			x++;
 			y++;
 		}
-		reference_pixel.PutPixel(x,y,color);
+		std::clog << "d: " << d << ", x: " << x  << ", y: " << y << std::endl;
+		line_reference_pixel.PutPixel(x,y,color);
 	}
 
 }
