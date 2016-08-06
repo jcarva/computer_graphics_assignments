0a65d13a2d20438811976611d979f0b8fc82308e
Jaelson <jaelsonjunior99@gmail.com>
2016-08-06 03:19:19 -0300

    Update comments and put the pixel inside the buffer line.

--- line.h
+++ line.h
@@ -34,51 +34,74 @@ void Line::DrawLine(std::string line_name, Pixel initial, Pixel final, int color
 
 
 	/*
-		Reference Pixel to draw the line, it will be
-		to puted a lot of time in different coordenates.
+		Reference Pixel to draw the line, it will be to puted a lot of time in different coordenates.
 	*/
-	Pixel line_reference_pixel = Pixel(0,0);
+	Pixel reference_pixel = Pixel(0,0);
 
 
 	/*
-		Respresentation of the reference and
-		complementary axes. They are necessary to
-		generalize the	Bresenham Algorithm to any
-		octante, so we need	change the 'while' stop
-		condition, and do octante transformations.
-	*/
-	int reference_axis;
-	int complementary_axis;
-
-	/*
-		Firsts Coordinates to PutPixel if we use
-		the Bresenham Algorithm's base case.
+		Firsts Coordinates to PutPixel when we use the Bresenham Algorithm's.
 	*/
 	int x = initial.column;
 	int y = initial.row;
 
 
 	/*
-		Beyonds line coordenates to generalize the
-		Bresenham Algorithm to any octante, so we need
-		change the 'while' stop condition.
+		Respresentation of the reference and complementary axes. They are necessary to generalize the
+		Bresenham Algorithm to any octante, so we need change the 'while' stop condition, and do octante
+		transformations.
+	*/
+	int * reference_axis;
+	int * complementary_axis;
+
+
+	/*
+		Beyond line coordenates to generalize the Bresenham Algorithm to any octante, so we need change
+		the 'while' stop condition.
 	*/
 	int x_limit;
 	int y_limit;
 
-	int deltaX = final.column - initial.column;
-	int deltaY = final.row - initial.row;
+
+	/*
+		Set beyond line coordenates.
+	*/
+	if (initial.column > final.column) //Set x limit
+		x_limit = (initial.column - final.column) + initial.column;
+	else
+		x_limit = final.column;
+
+	if (initial.row > final.row) //Set y limit
+		y_limit = (initial.row - final.row) + initial.row;
+	else
+		y_limit = final.row;
+
+	/*
+		Set deltaX, deltaY. According to the Bresenham Algorithm, these depend of the beyond line coordenates.
+	*/
+	int deltaX = x_limit - initial.column;
+	int deltaY = y_limit - initial.row;
+
+
+	/*
+		Variable that represents the 'while' limit to put pixels.
+	*/
+	int limit;
+	
+
 	int d = (2 * deltaY) - deltaX;
 	int increase_e = 2 * deltaY;
 	int increase_ne = 2 * (deltaY - deltaX);
 
-	std::clog << "###################" ;
-	std::clog << std::endl << "LINE: "<< line_name << std::endl;
-	std::clog << "dx: " << deltaX << std::endl << "dy: " << deltaY << std::endl;
-	std::clog << "increase_e: " << increase_e << std::endl << "increase_ne: " << increase_ne << std::endl << std::endl;
-
-	std::clog << "d: " << d << ", x: " << x  << ", y: " << y << std::endl;
-	line_reference_pixel.PutPixel(x,y,color);
+	// std::clog << "###################" ;
+	// std::clog << std::endl << "LINE: "<< line_name << std::endl;
+	// std::clog << "dx: " << deltaX << std::endl << "dy: " << deltaY << std::endl;
+	// std::clog << "increase_e: " << increase_e << std::endl << "increase_ne: " << increase_ne << std::endl << std::endl;
+	// std::clog << "d: " << d << ", x: " << x  << ", y: " << y << std::endl;
+	
+	reference_pixel.column = x;
+	reference_pixel.row = y;
+	buffer_line.push_back(reference_pixel);
 
 	while(x < final.column) {
 		if (d <= 0)
@@ -92,8 +115,19 @@ void Line::DrawLine(std::string line_name, Pixel initial, Pixel final, int color
 			x++;
 			y++;
 		}
-		std::clog << "d: " << d << ", x: " << x  << ", y: " << y << std::endl;
-		line_reference_pixel.PutPixel(x,y,color);
+
+
+		//std::clog << "d: " << d << ", x: " << x  << ", y: " << y << std::endl;
+		
+
+		reference_pixel.column = x;
+		reference_pixel.row = y;
+		buffer_line.push_back(reference_pixel);
+	}
+
+	for (int i = 0; i < buffer_line.size(); i++)
+	{
+		reference_pixel.PutPixel(buffer_line[i].column,buffer_line[i].row,color);
 	}
 
 }
